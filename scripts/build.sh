d71fdaab587fc28f002d089ce5cef517626a60b8
Vinita Vaidya <vaidyavinita.95@gmail.com>
2025-06-26 14:52:41 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
+
+# ✅ Fix: set default value for branch name
+BRANCH_NAME=${BRANCH_NAME:-latest}
+
 COMMIT=$(git rev-parse HEAD)
 echo "Building $COMMIT inside $(pwd)"
 
